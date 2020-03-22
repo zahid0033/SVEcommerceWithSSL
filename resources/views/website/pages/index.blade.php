305e9c6fc0412caf47d368000b49ed222bd424c3
zahid0033 <zahidhossain0033@gmail.com>
2020-03-22 12:36:18 +0600

    refats changes merge zahid08

--- resources/views/website/pages/index.blade.php
+++ resources/views/website/pages/index.blade.php
@@ -21,6 +21,253 @@
             <p class="text-center">Nobin Bangladesh was established by Mr. Anamul Hasan in 2001, is a fast growing Mobile Phone brand in Bangladesh and has been operating its business successfully for 19 years in the name of MOHAMMADIA TELECOM.in recent we have now NOBIN BANGLADESH is a sister concern of MOHAMMADIA TELECOM and workforce of  more than 50. Main products of Nobin Bangladesh are Television, Cellular Phone, Watch, Electrical Fan, LED Bulb. Nobin Bangladesh is the manufacturer and seller in the relevant industry and has gained high reputation in terms of its unbeatable capability for producing Electrical and Electronics goods in the most competitive way in aspect of quality, cost, design and innovation.</p>
         </div>
     </section>
+    <section>
+        <div class="section">
+            <!-- container -->
+            <div class="container">
+                <!-- row -->
+                <div class="row">
+                    <!-- section title -->
+                    <div class="col-md-12">
+                        <div class="section-title">
+                            <h2 class="title">Latest Products</h2>
+                        </div>
+                    </div>
+                    <!-- section title -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
+                                <img src="http://localhost/ecommerce_setcol/public/assets/img/product01.jpg" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50</h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                                <div class="product-btns">
+                                    <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                    <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                    <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <div class="product-label">
+                                    <span>New</span>
+                                    <span class="sale">-20%</span>
+                                </div>
+                                <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
+                                <img src="http://localhost/ecommerce_setcol/public/assets/img/product02.jpg" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                                <div class="product-btns">
+                                    <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                    <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                    <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <div class="product-label">
+                                    <span>New</span>
+                                    <span class="sale">-20%</span>
+                                </div>
+                                <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
+                                <img src="http://localhost/ecommerce_setcol/public/assets/img/product03.jpg" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                                <div class="product-btns">
+                                    <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                    <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                    <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <div class="product-label">
+                                    <span>New</span>
+                                </div>
+                                <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
+                                <img src="http://localhost/ecommerce_setcol/public/assets/img/product04.jpg" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50</h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                                <div class="product-btns">
+                                    <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                    <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                    <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <!-- banner -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="banner banner-2">
+                            <img src="http://localhost/ecommerce_setcol/public/assets/img/banner15.jpg" alt="">
+                            <div class="banner-caption">
+                                <h2 class="white-color">NEW<br>COLLECTION</h2>
+                                <button class="primary-btn">Shop Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /banner -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <div class="product-label">
+                                    <span>New</span>
+                                    <span class="sale">-20%</span>
+                                </div>
+                                <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
+                                <img src="http://localhost/ecommerce_setcol/public/assets/img/product07.jpg" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                                <div class="product-btns">
+                                    <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                    <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                    <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <div class="product-label">
+                                    <span>New</span>
+                                    <span class="sale">-20%</span>
+                                </div>
+                                <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
+                                <img src="http://localhost/ecommerce_setcol/public/assets/img/product06.jpg" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                                <div class="product-btns">
+                                    <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                    <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                    <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+
+                    <!-- Product Single -->
+                    <div class="col-md-3 col-sm-6 col-xs-6">
+                        <div class="product product-single">
+                            <div class="product-thumb">
+                                <div class="product-label">
+                                    <span>New</span>
+                                    <span class="sale">-20%</span>
+                                </div>
+                                <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
+                                <img src="http://localhost/ecommerce_setcol/public/assets/img/product05.jpg" alt="">
+                            </div>
+                            <div class="product-body">
+                                <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
+                                <div class="product-rating">
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star-o empty"></i>
+                                </div>
+                                <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
+                                <div class="product-btns">
+                                    <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                    <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                    <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product Single -->
+                </div>
+                <!-- /row -->
+            </div>
+            <!-- /container -->
+        </div>
+    </section>
     <section id="we_have" style="margin: 50px 0">
         <div class="container">
             <h1 class="text-center mt-5 mb-3">Products We Have</h1>
@@ -39,6 +286,51 @@
                 @endforeach
             </div>
 
+        </div>
+    </section>
+    <section>
+        <div class="section section-grey">
+            <!-- container -->
+            <div class="container">
+                <!-- row -->
+                <div class="row">
+                    <!-- banner -->
+                    <div class="col-md-8">
+                        <div class="banner banner-1">
+                            <img src="http://localhost/ecommerce_setcol/public/assets/img/banner13.jpg" alt="">
+                            <div class="banner-caption text-center">
+                                <h1 class="primary-color">HOT DEAL<br><span class="white-color font-weak">Up to 50% OFF</span></h1>
+                                <button class="primary-btn">Shop Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /banner -->
+
+                    <!-- banner -->
+                    <div class="col-md-4 col-sm-6">
+                        <a class="banner banner-1" href="#">
+                            <img src="http://localhost/ecommerce_setcol/public/assets/img/banner11.jpg" alt="">
+                            <div class="banner-caption text-center">
+                                <h2 class="white-color">NEW COLLECTION</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- /banner -->
+
+                    <!-- banner -->
+                    <div class="col-md-4 col-sm-6">
+                        <a class="banner banner-1" href="#">
+                            <img src="http://localhost/ecommerce_setcol/public/assets/img/banner12.jpg" alt="">
+                            <div class="banner-caption text-center">
+                                <h2 class="white-color">NEW COLLECTION</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- /banner -->
+                </div>
+                <!-- /row -->
+            </div>
+            <!-- /container -->
         </div>
     </section>
 
